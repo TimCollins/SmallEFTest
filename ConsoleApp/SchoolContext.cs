ea3e833472abd161dd896c3afe428006e0a7fe99
Tim Collins <tim.collins@hpe.com>
2016-03-14 17:09:43 +0000

    Added database context class.

---
+++ ConsoleApp/SchoolContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using ConsoleApp.Models;
+
+namespace ConsoleApp
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+    }
+}
