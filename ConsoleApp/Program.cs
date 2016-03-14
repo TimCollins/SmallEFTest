b16bd9f820f4c6d9ad0120750718630f5d0d4776
Tim Collins <tim.collins@hpe.com>
2016-03-14 17:12:43 +0000

    Added test code to create a student.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace ConsoleApp
+﻿using System;
+using ConsoleApp.Models;
+
+namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var ctx = new SchoolContext())
+            {
+                Student s = new Student
+                {
+                    StudentName = "Homer Simpson"
+                };
+
+                ctx.Students.Add(s);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("Student added.");
             Util.WaitForEscape();
         }
     }
